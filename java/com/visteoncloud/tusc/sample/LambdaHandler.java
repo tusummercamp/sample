d8ff089ed967e3d6a73295f5a9c88312e415ed50
Petar Vorotnikov <pvorotni@visteon.com>
2019-07-03 16:28:50 +0300

    Utilized aws-lambda-java-events Maven module

--- java/com/visteoncloud/tusc/sample/LambdaHandler.java
+++ java/com/visteoncloud/tusc/sample/LambdaHandler.java
@@ -3,16 +3,28 @@ package com.visteoncloud.tusc.sample;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class LambdaHandler implements RequestHandler<Request, Response>  {
+public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>  {
 
-	public Response handleRequest(Request input, Context context) {
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
 		
+		// get logger
 		LambdaLogger logger = context.getLogger();
-		logger.log("received person: " + input.getFirstName());
-
-		return new Response("Request received");
 		
+		// get body
+		String body = input.getBody();
+		
+		// log body
+		logger.log("Received request");
+		logger.log(body);
+		
+		// create and return response
+		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+		response.setStatusCode(200);
+		response.setBody("Response body comes here");
+		return response;
 	}
 
 }
