97da950b33b6d4ccfe4740ade974f38ade2b2be0
Petar Vorotnikov <pvorotni@visteon.com>
2019-07-05 10:58:13 +0300

    Added test class

--- src/main/java/com/visteoncloud/tusc/sample/LambdaHandler.java
+++ src/main/java/com/visteoncloud/tusc/sample/LambdaHandler.java
@@ -1,5 +1,7 @@
 package com.visteoncloud.tusc.sample;
 
+import org.json.JSONObject;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -14,20 +16,27 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
 		LambdaLogger logger = context.getLogger();
 		
 		// get body
-		String body = input.getBody();
+		JSONObject body = new JSONObject(input.getBody());
+		String foo = body.getString("foo");
+		Integer baz = body.getInt("baz");
 		
 		// log body
 		logger.log("Received request");
-		logger.log(body);
+		logger.log("Method " + input.getHttpMethod());
+		logger.log("Path " + input.getPath());
+		logger.log("Raw body " + input.getBody());
+		logger.log("foo " + foo);
+		logger.log("baz " + baz);
 		
-		String responseBody = "You created a request using ";
-		responseBody += input.getHttpMethod() + " method ";
-		responseBody += "and URL " + input.getResource();
+		// TODO: handle request data here
+		
+		JSONObject responseBody = new JSONObject();
+		responseBody.put("status", "ok");
 		
 		// create and return response
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
 		response.setStatusCode(200);
-		response.setBody(responseBody);
+		response.setBody(responseBody.toString());
 		return response;
 	}
 
